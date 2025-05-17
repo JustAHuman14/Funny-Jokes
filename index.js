0755de9b7a904dd694af427cf02bdb4b7e26be93
JustAHuman <kashyaparnav671@gmail.com>
2025-05-17 20:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function jokes() {
         let x = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart`)
         let y = await x.json()
-        let jokes =`<p style="display: flex; justify-content: center; align-items: center;">${y.setup}</p><br><br> <p style="display: flex; justify-content: center;">${y.delivery}<p>`
+        let jokes =`<p style="display: flex; justify-content: center; align-items: center;">${y.setup}</p><br><br> <p style="display: flex; justify-content: center;">${y.delivery}</p>`
         document.getElementById('jokes').innerHTML = jokes
         document.getElementById('jokes').style.color = 'black'
 }
